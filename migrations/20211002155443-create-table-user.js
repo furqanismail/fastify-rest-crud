68eef3782b04e86009460fcbe3176ea1a5bff2d5
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-03 23:43:57 +0800

    add migration user

--- migrations/20211002155443-create-table-user.js
+++ migrations/20211002155443-create-table-user.js
@@ -2,20 +2,25 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+
+     await queryInterface.createTable('user', {
+       id: {
+         type: Sequelize.INTEGER,
+         autoIncrement: true,
+         primaryKey: true,
+         allowNull: false
+       },
+       name: {
+         type: Sequelize.STRING,
+         allowNull: false
+       }
+     });
+
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+
+     await queryInterface.dropTable('user');
+
   }
 };
