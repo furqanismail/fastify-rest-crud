6b1b3173dd3a4481a47ac7fb883b078f60be662c
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-03 23:45:42 +0800

    update migration user

--- migrations/20211002155443-create-table-user.js
+++ migrations/20211002155443-create-table-user.js
@@ -13,7 +13,27 @@ module.exports = {
        name: {
          type: Sequelize.STRING,
          allowNull: false
-       }
+       },
+       email: {
+         type: Sequelize.STRING,
+         allowNull: false
+       },
+       password: {
+         type: Sequelize.STRING,
+         allowNull: false
+       },
+       address: {
+         type: Sequelize.STRING,
+         allowNull: false
+       },
+       created_at: {
+         type: Sequelize.DATE,
+         allowNull: false
+       },
+       updated_at: {
+         type: Sequelize.DATE,
+         allowNull: false
+       },
      });
 
   },
