1e1de30ed44cc269f272325c2a7109cc61b54e9c
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-10 20:50:59 +0800

    update route hero

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const heroRoutes = require('../controller/admin/hero')
 
 async function routes(fastify, options) {
     fastify.get('/hero', heroRoutes.get)
-    fastify.post('/hero', heroRoutes.create())
+    fastify.post('/hero', heroRoutes.create)
+    fastify.put('/hero/:id', heroRoutes.update)
+    fastify.delete('/hero/:id', heroRoutes.destroy)
 }
 
 module.exports = routes
