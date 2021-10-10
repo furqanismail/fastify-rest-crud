e7c55797a29a7272581e9ceea730d31f9cd33b6d
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-10 20:30:58 +0800

    update controller update hero

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const heroRoutes = require('../controller/admin/hero')
 
 async function routes(fastify, options) {
-
     fastify.get('/hero', heroRoutes.get)
     fastify.post('/hero', heroRoutes.create())
 }
