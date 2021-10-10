af7a15db2d7ce9a0ac525c1d8bbdc233aac58d00
furqaan ismail <furqaanismail.labfik@umi.ac.id>
2021-10-10 20:49:50 +0800

    update controller delete hero

--- controller/admin/hero/index.js
+++ controller/admin/hero/index.js
@@ -1,9 +1,11 @@
 const get = require('./get')
 const create = require('./create')
 const update = require('./update')
+const destroy = require('./delete')
 
 module.exports = {
     get,
     create,
-    update
+    update,
+    destroy
 }
